b7839e9f5e4c69d073a418b9430b094665b99bcf
王磊 <>
2022-12-19 21:16:26 +0800

    init

---
+++ High Rise/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Test
+//
+//  Created by 王磊 on 2018/12/8.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+}
+
---
+++ High Rise/SCNVector3 Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Test
+//
+//  Created by 王磊 on 2018/12/8.
+//
+
+import SceneKit
+
+extension SCNVector3 {
+  func absoluteValue() -> SCNVector3 {
+    return SCNVector3Make(abs(self.x), abs(self.y), abs(self.z))
+  }
+}
+
+func + (first: SCNVector3, second: SCNVector3) -> SCNVector3 {
+  return SCNVector3Make(first.x + second.x, first.y + second.y, first.z + second.z)
+}
+
+func - (first: SCNVector3, second: SCNVector3) -> SCNVector3 {
+  return SCNVector3Make(first.x - second.x, first.y - second.y, first.z - second.z)
+}
